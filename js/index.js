86a2dccd09f52a919ba609717b2d200aa788020f
Ac Espina <aeronespina0@gmail.com>
2023-08-11 14:14:21 +0800

    index.html modified in link css

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const email_input = document.getElementById('input');
+const subscribe_btn = document.querySelector('btn-submit');
+
+// subscribe_btn.addEventListener('click', ()=>{
+//     // const emailValue = email_input.value;
+//     // if(emailValue == null){
+//     //     alert("Please provide your email");
+//     // }
+//     alert("Please provide Email");
+// })
